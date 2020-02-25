7ed7646794b474f6110b1bbab3ed82d263f328eb
Ahmed Irfan <irfan@cs.stanford.edu>
2020-02-25 11:25:29 -0800

    update test

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,8 +1,12 @@
 #include "lbv2isolver.h"
 
+#include "smt-switch/msat_factory.h"
+
 using namespace lbv2i;
 
 int main() {
-  LBV2ISolver s = LBV2ISolver();
+  smt::SmtSolver underlying_solver = smt::MsatSolverFactory::create();
+
+  LBV2ISolver s = LBV2ISolver(s);
   //s.push();
 }
