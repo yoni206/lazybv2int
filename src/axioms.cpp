8c92b354aad63a01aa95c196f171eca16537f69f
Ahmed Irfan <irfan@cs.stanford.edu>
2020-02-28 20:30:51 -0800

    bvand-max lemma

--- src/axioms.cpp
+++ src/axioms.cpp
@@ -1,14 +1,43 @@
 #include "axioms.h"
 
+#include <math.h>
 #include <assert.h>
 
+using namespace std;
 using namespace smt;
 
 namespace lbv2i {
 
+static string pow2_minus_one_str(uint64_t k)
+{
+  assert(k <= 64);
+  assert(k >= 0);
+ 
+  uint64_t p = pow(2, k);
+  // make sure there is no overflow
+  assert(p > 0);
+
+  return to_string(p);
+}
+
+static void get_fbvand_args(Term f, uint64_t &bv_width, Term &a, Term &b)
+{
+  TermIter it = f->begin();
+  Term tmp = *it;
+
+  assert(tmp->is_value());
+  bv_width = tmp->to_int();
+  ++it;
+
+  a = *it;
+  ++it;
+  b = *it;
+}
+
 Axioms::Axioms(SmtSolver & solver) :
   solver_(solver)
 {
+  int_sort_ = solver_->make_sort(INT);
   false_ = solver_->make_term(false);
 }
 
@@ -18,24 +47,55 @@ Axioms::~Axioms()
 
 bool Axioms::check_bvand_base_case(Term t, TermVec &outlemmas)
 {
-  TermIter it = t->begin();
-  Term tmp = *it;
-
-  assert(tmp->is_value());
-  uint64_t bv_width = tmp->to_int();
-  ++it;
-
-  Term a = *it;
-  ++it;
-  Term b = *it;
+  uint64_t bv_width;
+  Term a, b;
+  get_fbvand_args(t, bv_width, a, b);
 
   if (bv_width == 1) {
     Term mul = solver_->make_term(Mult, a, b);
-    Term l = solver_->make_term(Equal, t, mul);
+    Term l = make_eq(t, mul);
     add_if_voilated(l, outlemmas);
   }
 
   return outlemmas.size() > 0;
+}
+
+bool Axioms::check_bvand_max(Term t, TermVec &outlemmas)
+{
+  uint64_t bv_width;
+  Term a, b;
+  get_fbvand_args(t, bv_width, a, b);
+
+  Term r = make_eq(t, a);
+  if (!b->is_value()) {
+    Term pre = make_eq(b, pow2_minus_one(bv_width));
+    Term l = make_implies(pre, r);
+    add_if_voilated(l, outlemmas);
+  } else {
+    uint64_t b_val = b->to_int();
+    if (b_val == (pow(2, bv_width) - 1)) {
+      add_if_voilated(r, outlemmas);
+    }
+  }
+
+  return outlemmas.size() > 0;
+}
+
+inline Term Axioms::pow2_minus_one(uint64_t k)
+{
+  string p = pow2_minus_one_str(k);
+  return solver_->make_term(p, int_sort_);
+}
+
+inline Term Axioms::make_eq(Term x, Term y)
+{
+  return solver_->make_term(Equal, x, y);
+}
+
+inline Term Axioms::make_implies(Term x, Term y)
+{
+  Term nx = solver_->make_term(Not, x);
+  return solver_->make_term(Or, nx, y);
 }
 
 inline void Axioms::add_if_voilated(Term l, TermVec &out)
--- src/axioms.h
+++ src/axioms.h
@@ -13,14 +13,23 @@ class Axioms
   // we assume that the term t looks like "fbvand(x, y)"
   // note the function name
   bool check_bvand_base_case(smt::Term t, smt::TermVec &outlemmas);
+  bool check_bvand_max(smt::Term t, smt::TermVec &outlemmas);
 
  private:
+
+  smt::Term pow2_minus_one(uint64_t k);
+
+  smt::Term make_eq(smt::Term x, smt::Term y);
+  smt::Term make_implies(smt::Term x, smt::Term y);
 
   void add_if_voilated(smt::Term l, smt::TermVec &out);
 
   smt::SmtSolver &solver_;
 
+  smt::Sort int_sort_;
+
   smt::Term false_;
+
 }; // class axioms
 
 } // namespace lbv2i
