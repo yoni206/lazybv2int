08af59b0e4d06fffcebed4f0100b3b3ba89d1707
yoni206 <yoni206@gmail.com>
2020-02-28 21:18:35 -0800

    shift

--- src/bv2int.h
+++ src/bv2int.h
@@ -28,12 +28,18 @@ class BV2Int : smt::IdentityWalker
   smt::Term make_range_constraint(smt::Term var, uint64_t bv_width);
   smt::Term make_bvnot_term(smt::Term x, uint64_t k);
   smt::Term int_max(uint64_t k);
-  smt::Term handle_bw_op(smt::Term t, uint64_t bv_width);
+  smt::Term handle_bw_op(smt::Term t, uint64_t bv_width, smt::TermVec cached_children);
   smt::Term handle_bw_op_lazy(smt::Term t, uint64_t bv_width);
-  smt::Term handle_bw_op_eager(smt::Term t, uint64_t bv_width);
+  smt::Term handle_bw_op_eager(smt::Term t, uint64_t bv_width, smt::TermVec cached_children);
+  smt::Term handle_shift_eager(smt::Term t, uint64_t bv_width, smt::TermVec cached_children);
+  smt::Term handle_boolean_bw_eager(smt::Term t, uint64_t bv_width);
+  bool is_shift_op(smt::Op op);
+
+
+
   bool is_bw_op(smt::Op op);
   bool abstract_;
-
+  
   smt::TermVec range_assertions_;
   smt::TermVec sigma_vars_;
 
