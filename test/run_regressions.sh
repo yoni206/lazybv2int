435f57887af2688142c5419ea64ef13cf2902150
yoni206 <yoni206@gmail.com>
2020-03-11 22:16:50 -0700

    initial regression script

---
+++ test/run_regressions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+MY_DIR="`dirname \"$0\"`"
+MY_DIR=`realpath $MY_DIR`
+CVC4_PATH=$MY_DIR/../deps/smt-switch/deps/CVC4/build/bin/cvc4
+LAZY_PATH=$MY_DIR/../build/lazybv2int
+SMTLIB_FILES_DIR=$MY_DIR/smtlib_files
+
+for f in `find $SMTLIB_FILES_DIR -name "*.smt2"`
+do
+  cvc4_res=`runlim -t 3 --output-file=tmp1.log $CVC4_PATH $f`
+  lazy_res=`runlim -t 3 --output-file=tmp2.log $LAZY_PATH $f`
+  if [ "$cvc4_res" = "sat" -a "$lazy_res" = "unsat" ] 
+  then
+    echo $f FAIL
+#  else
+#    echo $f PASS
+  fi
+  
+  if [ "$cvc4_res" = "unsat" -a "$lazy_res" = "sat" ] 
+  then
+    echo $f FAIL
+#  else
+#    echo $f PASS
+  fi
+done
