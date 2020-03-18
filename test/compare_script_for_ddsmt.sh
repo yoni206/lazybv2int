013370d645ac1e7ba3b222a3ec9fa187e73bf7c7
yoni206 <yoni206@gmail.com>
2020-03-18 15:18:35 -0700

    scripts to minimize problems

---
+++ test/compare_script_for_ddsmt.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#minimize difference from cvc4
+MY_DIR="`dirname \"$0\"`"
+MY_DIR=`realpath $MY_DIR`
+CVC4_PATH=$MY_DIR/../deps/smt-switch/deps/CVC4/build/bin/cvc4
+LAZY_PATH=$MY_DIR/../build/lazybv2int
+f=$1
+
+#store the result of cvc4
+cvc4_res=`$CVC4_PATH $f`
+
+#store the result of bv2int. Change this line to invoke different options.
+lbv2int_res=`$LAZY_PATH $f --msat --lazy --full-refinement`
+
+if [ "$lbv2int_res" = "sat" -a "$cvc4_res" = "unsat" ]
+then
+  echo different
+  return 1
+fi
+
+if [ "$lbv2int_res" = "unsat" -a "$cvc4_res" = "sat" ]
+then
+  echo different
+  return 1
+fi
+echo same
+return 0
---
+++ test/minimize_diff.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+MY_DIR="`dirname \"$0\"`"
+MY_DIR=`realpath $MY_DIR`
+DDSMT_PATH=$MY_DIR/../deps/ddSMT/ddsmt.py
+RUNNING_SCRIPT_PATH=$MY_DIR/compare_script_for_ddsmt.sh
+f=$1
+
+$DDSMT_PATH --match-out different $f $f.minimized.smt2 $RUNNING_SCRIPT_PATH
