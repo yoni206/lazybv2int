62596a383fcccfe1858e4a87e2336555a4fd6306
Makai Mann <makaim@stanford.edu>
2020-03-25 11:49:31 -0700

    Add debug option to configure

--- configure.sh
+++ configure.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
+
+usage () {
+cat <<EOF
+Usage: $0 [<option> ...]
+
+Configures the CMAKE build environment.
+
+-h, --help              display this message and exit
+--debug                 build debug with debug symbols (default: off)
+EOF
+  exit 0
+}
+
+die () {
+    echo "*** $0: $*" 1>&2
+    exit 1
+}
+
+build_type=Release
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -h|--help) usage;;
+        --debug)
+            build_type=Debug;
+            ;;
+        *) die "unexpected argument: $1";;
+    esac
+    shift
+done
+
+cmake_opts="-DCMAKE_BUILD_TYPE=$build_type"
+
 mkdir -p build
 cd build || exit 1
-cmake ..
+cmake .. $cmake_opts 2>&1
 cd ..
