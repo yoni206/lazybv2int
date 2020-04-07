f8e8173d9aefe22b919c5893909c82f9cc7540de
Makai Mann <makaim@stanford.edu>
2020-04-06 17:02:29 -0700

    Static option and use static smt-switch

--- configure.sh
+++ configure.sh
@@ -9,6 +9,8 @@ Configures the CMAKE build environment.
 
 -h, --help              display this message and exit
 --debug                 build debug with debug symbols (default: off)
+--static-lib            build a static library (default: off)
+--static                build a static executable, automatically sets --static-lib also (default: off)
 EOF
   exit 0
 }
@@ -19,6 +21,9 @@ die () {
 }
 
 build_type=Release
+lib_type=SHARED
+static_opt=""
+static_exec=NO
 
 while [ $# -gt 0 ]
 do
@@ -27,14 +32,24 @@ do
         --debug)
             build_type=Debug;
             ;;
+        --static-lib)
+            lib_type=STATIC;
+            ;;
+        --static)
+            static_exec=YES;
+            lib_type=STATIC;
+            ;;
         *) die "unexpected argument: $1";;
     esac
     shift
 done
 
 cmake_opts="-DCMAKE_BUILD_TYPE=$build_type"
+cmake_opts="${cmake_opts} -DLBV2I_LIB_TYPE=${lib_type}"
+cmake_opts="${cmake_opts} -DLBV2I_STATIC_EXEC=${static_exec}"
 
 mkdir -p build
 cd build || exit 1
+echo "configuring with: ${cmake_opts}"
 cmake .. $cmake_opts 2>&1
 cd ..
