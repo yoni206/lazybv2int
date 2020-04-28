fdd04c94a757dbc5026b9854d63649f784d3569e
Ahmed Irfan <irfan@cs.stanford.edu>
2020-04-28 14:34:01 -0700

    left shift zero axiom

--- src/axioms.cpp
+++ src/axioms.cpp
@@ -290,7 +290,40 @@ bool Axioms::check_bvand_bnw(const Term & t, TermVec & outlemmas)
 
 bool Axioms::check_bvshiftl_zero(const Term & t, TermVec & outlemmas)
 {
-  return false;
+  const size_t n = outlemmas.size();
+  uint64_t bv_width;
+  Term a, b;
+  get_fbv_args(t, bv_width, a, b);
+
+  // b = 0 -> t = a
+  Term b_eq_zero = make_eq(b, zero_);
+  Term t_eq_a = make_eq(t, a);
+  Term l = make_implies(b_eq_zero, t_eq_a);
+  add_if_voilated(l, outlemmas);
+
+  if (outlemmas.size() == n) {
+    // t = a -> b = 0
+    l = make_implies(t_eq_a, b_eq_zero);
+    add_if_voilated(l, outlemmas);
+  }
+
+  if (outlemmas.size() == n) {
+    // b >= bv_width -> t = 0
+    l = make_implies(t_eq_a, b_eq_zero);
+    add_if_voilated(l, outlemmas);
+  }
+
+  if (outlemmas.size() == n) {
+    // t = 0 -> (a = 0 \/ b >= bv_width)
+    Term t_eq_zero = make_eq(t, zero_);
+    Term a_eq_zero = make_eq(a, zero_);
+    Term cases = solver_->make_term(Or, a_eq_zero,
+                                    solver_->make_term(Ge, b, solver_->make_term(to_string(bv_width), int_sort_)));
+    l = make_implies(t_eq_zero, cases);
+    add_if_voilated(l, outlemmas);
+  }
+
+  return outlemmas.size() > n;
 }
 
 inline Term Axioms::pow2_minus_one(uint64_t k)
