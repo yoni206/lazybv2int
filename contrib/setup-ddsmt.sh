1e786a989caa7923aa5ad1efed53d2987606e741
yoni206 <yoni206@gmail.com>
2020-05-19 15:27:07 -0700

    ddsmt is on the user to get

--- contrib/setup-ddsmt.sh
+++ contrib/setup-ddsmt.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
-
-usage () {
-    cat <<EOF
-Usage: $0 [<option> ...]
-
-Sets up ddsmt.
-
--h, --help              display this message and exit
-EOF
-    exit 0
-}
-
-while [ $# -gt 0 ]
-do
-    case $1 in
-        -h|--help) usage;;
-        --debug)
-            CONF_OPTS=--debug;;
-        *) die "unexpected argument: $1";;
-    esac
-    shift
-done
-
-mkdir -p $DEPS
-
-if [ ! -d "$DEPS/ddsmt" ]; then
-    cd $DEPS
-    git clone https://github.com/aniemetz/ddSMT.git
-    cd ddSMT
-    cd $DIR
-else
-    echo "$DEPS/ddsmt already exists. If you want to rebuild, please remove it manually."
-fi
+echo get ddSMT from https://github.com/aniemetz/ddSMT.git and clone it into `realpath $DEPS`/ddSMT
